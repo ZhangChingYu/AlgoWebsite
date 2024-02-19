fdb49afd860242fa885e9e5b0aef6649e56175b5
Silvia Chang <sunny1218.chang@gmail.com>
2024-02-19 23:37:46 +1100

    initialize

---
+++ algowebsite/src/pages/home/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './home.css';
+import Logo from '../../assets/Logo.jpg'
+import { Items } from "../../components/home";
+import { CopyRight } from "../../components";
+
+var aData = [{en:"Array", zn:"列隊"}, {en:"Linked List", zn:"鏈表"}, {en:"Stacks", zn:"堆棧"}, {en:"Queues", zn:"佇列"}, {en:"Heaps", zn:"堆"}, {en:"Heaps", zn:"堆"}, {en:"Heaps", zn:"堆"}];
+var dData = [{en:"Array", zn:"列隊"}, {en:"Linked List", zn:"鏈表"}, {en:"Stacks", zn:"堆棧"}, {en:"Queues", zn:"佇列"}, {en:"Heaps", zn:"堆"}, {en:"Heaps", zn:"堆"}, {en:"Heaps", zn:"堆"}];
+
+const Home = () => {
+    
+    return(
+        <div className="home">
+            <div className="home_content">
+                <div className="home_header">
+                    <img className="home_logo" src={Logo} alt="logo"/>
+                </div>
+                <div className="home_body">
+                    <div className="home_section">
+                        <h1>Data Structure</h1>
+                        <Items data={dData}/>
+                    </div>
+                    <div className="home_section">
+                        <h1>Algorithms</h1>
+                        <Items data={aData}/>
+                    </div>
+                </div>
+            </div>
+            <CopyRight/>
+        </div>
+    )
+}
+
+export default Home;
