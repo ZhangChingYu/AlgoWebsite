e7354f486518b9bf7f1e0985174aa2fa0d05facb
Silvia Chang <sunny1218.chang@gmail.com>
2024-02-20 15:54:12 +1100

    degrading react from 18 to 17

--- algowebsite/src/main.jsx
+++ algowebsite/src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom'; // 正確引入 ReactDOM
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render( // 使用 ReactDOM.render() 方法
   <React.StrictMode>
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
